80f9ec162b9ad0d6808ea6f1d423fa1b3e69b5f1
Morteza Ansarinia <ansarinia@me.com>
2012-01-12 18:57:08 +0330

    add remote fetching from thebirdy new purchase page

--- injection.js
+++ injection.js
@@ -6,9 +6,9 @@ function ahah(url, target) {
   document.getElementById(target).innerHTML = ' Loading The Birdy...';
   if (window.XMLHttpRequest) {
     req = new XMLHttpRequest();
-  }/* else if (window.ActiveXObject) {
+  } else if (window.ActiveXObject) {
     req = new ActiveXObject("Microsoft.XMLHTTP");
-  }*/
+  }
   if (req != undefined) {
     req.onreadystatechange = function() {ahahDone(url, target);};
     req.open("GET", url, true);
